f58c993c4711f2b0e0bd8d42824472de41eb9e8a
Beylikov <162538900+Beylikov@users.noreply.github.com>
2025-05-04 21:34:05 +0800

    Add checksum utilities

--- sr1.c
+++ sr1.c
@@ -18,3 +18,20 @@ void B_init(void)              {}
 void B_input(packet pkt)       {}
 void B_output(message msg)     {}
 void B_timerinterrupt(void)    {}
+/* 计算并返回 pkt 的校验和 */
+unsigned short ComputeChecksum(packet *pkt) {
+    unsigned long sum = 0;
+    unsigned short *data = (unsigned short *)pkt;
+    for (int i = 0; i < sizeof(packet)/2; i++) {
+        sum += data[i];
+        if (sum & 0xFFFF0000) {
+            sum = (sum & 0xFFFF) + (sum >> 16);
+        }
+    }
+    return (unsigned short)(~sum);
+}
+
+/* 判断 pkt 是否损坏 */
+bool IsCorrupted(packet *pkt) {
+    return ComputeChecksum(pkt) != pkt->checksum;
+}
